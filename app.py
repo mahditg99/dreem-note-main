daa4e686c0fee3b237d6c0a94f7065afd57df350
mahditg99 <mahditwork99@gmail.com>
2025-05-31 21:57:20 +0000

    init

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
 from flask_sqlalchemy import SQLAlchemy
